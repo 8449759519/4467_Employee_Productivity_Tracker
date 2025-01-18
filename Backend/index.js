b77332fe26ce5da93f4afa3797f8bd03bb70aace
Shrey Bhatt <ab422380@gmail.com>
2025-01-18 14:11:37 +0530

    n

--- Backend/index.js
+++ Backend/index.js
@@ -2,19 +2,23 @@ import express from "express";
 import { connect } from "mongoose";
 import cors from "cors";
 import { userRouter } from "./router/userRouter.js";
-let app = express();
+
+const app = express();
+
 app.use(express.json());
-app.use("/user", userRouter);
-// app.use(
-//   cors({
-//     origin: "http://127.0.0.1:5500",
-//   })
-// );
 
 app.use(cors({ origin: "*" }));
+
+app.use("/user", userRouter);
+
 app.listen(5000, async () => {
-  let connection = await connect(
-    "mongodb+srv://ab422380:newPassword@cluster0.ckd1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
-  console.log("server has started on port 5000");
+  try {
+    // Connect to MongoDB
+    await connect(
+      "mongodb+srv://ab422380:newPassword@cluster0.ckd1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log("Connected to MongoDB and server started on port 5000");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
 });
