d03c5423c86b99fb0aaf04a0a7b30e9aabde69b6
Shrey Bhatt <ab422380@gmail.com>
2025-01-18 14:04:59 +0530

    n

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { connect } from "mongoose";
+import cors from "cors";
 import { userRouter } from "./router/userRouter.js";
 let app = express();
 app.use(express.json());
 app.use("/user", userRouter);
+// app.use(
+//   cors({
+//     origin: "http://127.0.0.1:5500",
+//   })
+// );
 
+app.use(cors({ origin: "*" }));
 app.listen(5000, async () => {
   let connection = await connect(
     "mongodb+srv://ab422380:newPassword@cluster0.ckd1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
